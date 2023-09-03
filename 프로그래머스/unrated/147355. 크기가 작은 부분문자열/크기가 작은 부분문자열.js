d5da5d1f27daea80ec8c32924410b09111cacc34
rorobin <87680494+ChoiTobin@users.noreply.github.com>
2023-09-04 08:18:11 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -13,13 +13,7 @@ let result
     }
     answer= result.length
    
-    
 
-    
-  
     
     return answer;
 }
-
-//t를 for문을 돌린다. 처음에 0~2 앞에것도 +1 뒤에것도 +1 해서 배열에 넣고
-//map을 돌린후 item을 filter로 하여 p보다 작거나 큰것 만남긴후에 그놈의 length를 반환.
